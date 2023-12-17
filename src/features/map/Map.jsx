be556646b9decb097b2ced68466d3a981adeda17
Zawalid <walid.zakan@gmail.com>
2023-12-17 20:42:25 +0100

    Add a satellite view to the map

--- src/features/map/Map.jsx
+++ src/features/map/Map.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import Loader from '@/ui/Loader';
@@ -10,6 +10,10 @@ import { LocationMarker } from './LocationMarker';
 import { CitiesConnection } from './CitiesConnection';
 import ZoomControl from './ZoomControl';
 import CustomMarker from './CustomMarker';
+
+import '@maptiler/leaflet-maptilersdk';
+import L from 'leaflet';
+import IconButton from '../../ui/IconButton';
 
 const locationFallback = [51.505, -0.09]; // London
 
@@ -26,6 +30,7 @@ export default function Map() {
     enableScrollZoom,
     enableDoubleClickZoom,
   } = useSettings();
+  const [isSatellite, setIsSatellite] = useState(false);
 
   const { latitude, longitude } = location.state || {};
 
@@ -54,7 +59,11 @@ export default function Map() {
         doubleClickZoom={enableDoubleClickZoom}
         ref={ref}
       >
-        <TileLayer url='https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png' />
+        {isSatellite ? (
+          <SatelliteLayer />
+        ) : (
+          <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
+        )}
         {myCities.map((city) => {
           return (
             <CustomMarker
@@ -73,6 +82,9 @@ export default function Map() {
           <div className='flex flex-col gap-1'>
             <LocationMarker />
             <CitiesConnection positions={positions} />
+            <IconButton type={3} onClick={() => setIsSatellite(!isSatellite)}>
+              <i className='fa-solid fa-satellite'></i>
+            </IconButton>
           </div>
         </div>
       </MapContainer>
@@ -83,5 +95,22 @@ export default function Map() {
 function ChangeCenter({ position }) {
   const map = useMap();
   map.flyTo(position, 13);
+  return null;
+}
+
+function SatelliteLayer() {
+  const map = useMap();
+  const url =
+    'https://api.maptiler.com/tiles/satellite-v2/{z}/{x}/{y}.jpg?key=qugdCTkMu3TjQNgzToSq';
+
+  L.tileLayer(url, {
+    tileSize: 512,
+    zoomOffset: -1,
+    minZoom: 1,
+    attribution:
+      '\u003ca href="https://www.maptiler.com/copyright/" target="_blank"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href="https://www.openstreetmap.org/copyright" target="_blank"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e',
+    crossOrigin: true,
+  }).addTo(map);
+
   return null;
 }
