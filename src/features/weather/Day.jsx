8edc6d01fa2af5b6d3bf047394f9e8d3ce8a5e92
Zawalid <walid.zakan@gmail.com>
2023-12-18 16:47:51 +0100

    Change the weather icons Remove the sign up component Add a flag to the plan component

--- src/features/weather/Day.jsx
+++ src/features/weather/Day.jsx
@@ -2,12 +2,13 @@ import { getWeatherImageAndDescription } from '../../utils/helpers';
 
 export default function Day({ day, weatherCode, temperature }) {
   const description = getWeatherImageAndDescription(weatherCode)?.description;
+  const isDay = new Date().getHours() > 6 && new Date().getHours() < 18;
   return (
     <div className='grid grid-cols-[50px_1fr_1fr] items-center justify-between gap-3 border-b border-border pb-2 last:border-none last:pb-0'>
       <span className='noTextWrap text-sm  text-text-secondary'>{day}</span>
       <div className='grid grid-cols-[50px_1fr] items-center gap-4'>
         <img
-          src={getWeatherImageAndDescription(weatherCode)?.image}
+          src={getWeatherImageAndDescription(weatherCode, isDay)?.image}
           alt={description}
           className='w-full'
         />
