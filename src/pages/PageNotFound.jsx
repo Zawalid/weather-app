676afeba97f27786184fccca22b9557ce3052fbc
Zawalid <walid.zakan@gmail.com>
2023-12-18 21:19:08 +0100

    Update the page not found component Add robots file to public folder

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 export default function PageNotFound() {
   useEffect(() => {
     document.title = 'Breezy | 404';
+
+    return () => document.title = 'Breezy | Never forget your umbrella';
   }, []);
 
   return (
     <div className='grid h-screen place-content-center'>
-      <img src='/404.svg' alt='404' className='mx-auto w-1/2' />
-      <h1 className='mt-5 text-center font-medium text-text-primary'>
-        Oops! It seems you lost your umbrella
+      <img src='/404.svg' alt='404' className='mx-auto w-2/5' />
+      <h1 className='mt-5 text-2xl text-center font-medium text-text-primary'>
+        Oops! It seems you&apos;re lost.
       </h1>
 
       <Link
